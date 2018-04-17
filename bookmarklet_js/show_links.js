185230a3948eb52d80da4947e64aa4a5ce34087e
Kathy Eng <kengdoj@users.noreply.github.com>
2018-04-17 16:18:33 -0400

    Create show_links.js

---
+++ bookmarklet_js/show_links.js
@@ -0,0 +1,2 @@
+(function() {
+for(l=0;l<document.all.length;l++){if(document.all[l].tagName=='A'){with(document.all[l].style){if(backgroundColor=='lime'){void(backgroundColor=document.bgColor)}else{void(backgroundColor='lime')}}}};
