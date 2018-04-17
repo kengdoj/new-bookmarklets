881c6ee27ea3aa494e5f19e74b3716da885d7c77
Kathy Eng <kengdoj@users.noreply.github.com>
2018-04-17 16:28:18 -0400

    Create flash_rect.js

---
+++ bookmarklet_js/flash_rect.js
@@ -0,0 +1,50 @@
+(function on() {
+var block_to_insert;
+var insert_this;
+block_to_insert= document.createElement = ('div');
+block_to_insert.innerHTML = '<canvas id=\"myCanvas\" width=\"341\" height=\"256\" style=\"border:1px solid #000000; background:#8080C0;\">' ;
+block_to_insert.id = 'mydiv';
+}
+
+//Make the DIV element draggable:
+dragElement(document.getElementById(("mydiv")));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    /* if present, the header is where you move the DIV from:*/
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    /* otherwise, move the DIV from anywhere inside the DIV:*/
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    /* stop moving when mouse button is released:*/
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+)();
