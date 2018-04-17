59fea8b961432c14f5ec002cc9c44dc0a542362a
Kathy Eng <kengdoj@users.noreply.github.com>
2018-04-17 16:23:36 -0400

    Create icon_fonts.js

---
+++ bookmarklet_js/icon_fonts.js
@@ -0,0 +1,1 @@
+(function(){var sheet=document.createElement('style');sheet.innerHTML="i, .fa, .glyphicon, .material-icons, [data-icon] { background-color: lime; }";document.body.appendChild(sheet);})();
