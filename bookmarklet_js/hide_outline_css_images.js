e4ea198f83a984b56e7dfae73ac48f356ee1a265
Kathy Eng <kengdoj@users.noreply.github.com>
2018-04-17 16:03:28 -0400

    Update hide_outline_css_images.js

--- bookmarklet_js/hide_outline_css_images.js
+++ bookmarklet_js/hide_outline_css_images.js
@@ -1,1 +1,28 @@
-(function(){var tags=document.getElementsByTagName('*');var element;for(var i=0;i<tags.length;i++){element=tags[i];if(element.currentStyle){if(element.currentStyle['backgroundImage']!=='none')element.style.outline='2px solid #f00';}else if(window.getComputedStyle){if(document.defaultView.getComputedStyle(element,null).getPropertyValue('background-image')!=='none')element.style.outline='2px solid #f00';}}if(confirm("Do you want to hide the background images?")==true){for(var i=0;i<tags.length;i++){element=tags[i];if(element.currentStyle){if(element.currentStyle['backgroundImage']!=='none')element.style.backgroundImage='none';}else if(window.getComputedStyle){if(document.defaultView.getComputedStyle(element,null).getPropertyValue('background-image')!=='none')element.style.backgroundImage='none';}}}else{alert("Background images will be outlined.");}})()
+(function(){
+  var tags=document.getElementsByTagName('*');
+  var element;
+  for(var i=0;i<tags.length;i++){
+    element=tags[i];
+    if(element.currentStyle) {
+      if(element.currentStyle['backgroundImage']!=='none')element.style.outline='2px solid #f00';
+      } else if(window.getComputedStyle) {
+            if(document.defaultView.getComputedStyle(element,null).getPropertyValue('background-image')!=='none')
+              element.style.outline='2px solid #f00';
+            }
+    }
+  if(confirm("Do you want to hide the background images?")==true) {
+    for(var i=0;i<tags.length;i++) {
+        element=tags[i];
+        if(element.currentStyle) { 
+            if(element.currentStyle['backgroundImage']!=='none')element.style.backgroundImage='none';
+        } else if(window.getComputedStyle) {
+          if(document.defaultView.getComputedStyle(element,null).getPropertyValue('background-image')!=='none')
+            element.style.backgroundImage='none';
+        }
+    }
+  } else {
+    alert("Background images will be outlined.");
+  }
+}
+)
+()
