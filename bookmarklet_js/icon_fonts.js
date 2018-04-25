eac34a1866aba37acf461307702c90d0e88d61a5
Kathy Eng <kengdoj@users.noreply.github.com>
2018-04-24 21:47:47 -0400

    Update icon_fonts.js

--- bookmarklet_js/icon_fonts.js
+++ bookmarklet_js/icon_fonts.js
@@ -1,6 +1,6 @@
 (function(){
-  var sheet=document.createElement('style');
-  sheet.innerHTML=('i, .fa, .glyphicon, .material-icons, [data-icon] { background-color: lime; }');
-  document.body.appendChild(sheet);
+  //var sheet=document.createElement('style');
+  //sheet.innerHTML=('i, .fa, .glyphicon, .material-icons, [data-icon] { background-color: lime; }');
+  //document.body.appendChild(sheet);
   alert("Icon fonts have been highlighted if found.");
 })();
