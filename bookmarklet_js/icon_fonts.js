8df7b4bba3d6bb7fa07f5faa77dbdde21bf01fe5
Kathy Eng <kengdoj@users.noreply.github.com>
2018-04-24 20:45:07 -0400

    Update icon_fonts.js

--- bookmarklet_js/icon_fonts.js
+++ bookmarklet_js/icon_fonts.js
@@ -2,5 +2,5 @@
   var sheet=document.createElement('style');
   sheet.innerHTML="i, .fa, .glyphicon, .material-icons, [data-icon] { background-color: lime; }";
   document.body.appendChild(sheet);
-  alert("Icon fonts have been highlighted (if found).");
+  alert("Icon fonts have been highlighted if found.");
 })();
